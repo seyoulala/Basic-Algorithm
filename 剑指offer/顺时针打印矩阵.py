d8ed6e447031b200a62857e52081251287d65b9d
徐英浩 <xuyinghao@51xf.cn>
2019-03-07 17:04:16 +0800

    add a article or modify article

--- 剑指offer/顺时针打印矩阵.py
+++ 剑指offer/顺时针打印矩阵.py
@@ -5,37 +5,36 @@
 import  numpy as np
 
 class Solution:
+    def spiralOrder(self, matrix):
+        if len(matrix)==0:
+            return []
 
-    def PrintMatrix(self,Matrxi):
-        if len(Matrxi)<=0:
-            pass
-
-        left = 0
-        right = Matrxi.shape[1]
         top = 0
-        bottom = Matrxi.shape[0]
+        left = 0
+        right = len(matrix[0])-1
+        bottom = len(matrix)-1
         result = []
-        while left<=right and top <= bottom:
-            #从左向右遍历
-            for i in range(left,right):
-                result.append(Matrxi[top][i])
-            #从上到下遍历
-            for i in range(top+1,bottom):
-                result.append(Matrxi[i][right-1])
-            #如果要从右边到左边,肯定top！=bottom
-            if top != bottom:
-                for i in range(left,right-1)[::-1]:
-                    result.append(Matrxi[bottom-1][i])
-            #如果要从下到上，肯定从右到左边走过了，那么left！=right
-            if left != right:
-                for i in range(top+1,bottom-1)[::-1]:
-                    result.append(Matrxi[i][left])
-            left +=1
-            right -=1
-            top +=1
-            bottom -=1
-        return result
+        #逆时针打印矩阵
+        while left<=right and top<=bottom:
+            for i in range(left,right+1):
+                result.append(matrix[top][i])
+            #从上往下打印
+            for i in range(top+1,bottom+1):
+                result.append(matrix[i][right])
+            #从右往左边打印
+            if top !=bottom:
+                for i in range(left,right)[::-1]:
+                    result.append(matrix[bottom][i])
+            if left!=right:
+                for i in range(top+1,bottom)[::-1]:
+                    result.append(matrix[i][left])
 
+            left+=1
+            right-=1
+            top+=1
+            bottom-=1
+
+        return  result
 
 test_data = np.arange(1,17).reshape(4,4)
 s= Solution()
