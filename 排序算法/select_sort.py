6be5783f9b105502f52a35112c078d81c363c882
Ethan <35033183+seyoulala@users.noreply.github.com>
2019-03-17 15:13:12 +0800

    Update select_sort.py

--- 排序算法/select_sort.py
+++ 排序算法/select_sort.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import  numpy as np
 def select_sort(input_list):
+	
 	"""
 	函数说明；选择排序是从待排序的序列中选出最小的关键字
 	如果最小元素不是待排序的第一位则交换。从余下的N-1个元素中
@@ -8,20 +9,17 @@ def select_sort(input_list):
 	:param input_list: 待排序序列
 	:return:排序后的序列
 	"""
-	length = len(input_list)
-	if length <= 1:
-		return input_list
-	sort_list = input_list
-	for i in range(0,length):
-		min_index = i #初始化
-		for j in range(i+1,length):
-			if sort_list[min_index] > sort_list[j]:
-				min_index = j
-		temp = sort_list[i]
-		sort_list[i] = sort_list[min_index]
-		sort_list[min_index] = temp
-
-	return sort_list
+    if array is None or len(array) < 2:
+        return
+    length = len(array)
+    for i in range(length - 1):
+        min_index = i
+        for j in range(i + 1, length):
+            if array[j] < array[min_index]:
+                min_index = j
+        # 将当前位置的数和最小的数字交换
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
 
 if __name__ == '__main__':
 	input_list = list(np.random.randint(0,100,size=100))
