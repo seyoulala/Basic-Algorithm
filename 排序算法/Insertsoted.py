9fc5ee7d0943ea4f5d8c566b2a7bbba5bae384ce
Ethan <35033183+seyoulala@users.noreply.github.com>
2019-03-17 15:02:56 +0800

    Update Insertsoted.py

--- 排序算法/Insertsoted.py
+++ 排序算法/Insertsoted.py
@@ -3,18 +3,16 @@
 #当序列接近正序的时候排序最好,0(n),当序列接近反序的时候,时间复杂度为O(n**2)
 #在交换的时候需要0(1)一个空间来保存,对于相等的数字不会交换,因此是稳定的排序
 def insertSoted(input_list):
-	if len(input_list) == 0:
-		return []
-	sorted_list = input_list
-	for i in range(1,len(input_list)):
-		tempt = sorted_list[i]
-		j = i-1
-		while j >= 0 and tempt <sorted_list[j]:
-			sorted_list[j + 1] = sorted_list[j]
-			j -=1
-			sorted_list[j+1] = tempt
-		print(sorted_list)
-	return sorted_list
+    if array is None or len(array) < 2:
+        return
+    # 将当前位置的数插入到已排序数组的合适位置
+    length = len(array)
+    for i in range(1, length):
+        j = i - 1
+        while j >= 0 and array[j] > array[j + 1]:
+            array[j + 1], array[j] = array[j], array[j + 1]
+            j -= 1
+    return array
 if __name__ =='__main__':
 	input_list = [6,4,8,9,2,3,1]
 	print('排序前',input_list)
