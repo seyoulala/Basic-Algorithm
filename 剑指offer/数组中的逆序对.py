fbf27dc198ef0ab3c07ad9a94294c8f6664c0201
Ethan <35033183+seyoulala@users.noreply.github.com>
2019-03-23 19:57:27 +0800

    Update 数组中的逆序对.py

--- 剑指offer/数组中的逆序对.py
+++ 剑指offer/数组中的逆序对.py
@@ -5,42 +5,112 @@
 # @Link    : https://github.com/seyoulala/Basic-Algorithm
 # @Version : $Id$
 
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
+import random
+import copy
 class Solution:
-    def IsBalanced_Solution(self, pRoot):
-        # write code here
-        if pRoot is None:
-            return True
-        if abs(self.TreeDepth(pRoot.left) - self.TreeDepth(pRoot.right)) > 1:
-            return False
-        return self.IsBalanced_Solution(pRoot.left) and self.IsBalanced_Solution(pRoot.right)
+	def InversePairs(self, arr):
+		if arr is None or len(arr) < 2:
+			return 0
+		return self.mergeSort(arr, 0, len(arr) - 1)
 
-    def TreeDepth(self, pRoot):
-        if pRoot is None:
-            return 0
-        return max(self.TreeDepth(pRoot.left), self.TreeDepth(pRoot.right)) + 1
+	def mergeSort(self, arr, l, r):
+		if l == r:
+			return 0
+		# 防止l+r溢出
+		mid = l + ((r - l) >> 1)
+		return self.mergeSort(arr, l, mid) + self.mergeSort(arr, mid + 1, r) + self.merge(arr, l, mid, r)
 
-if __name__=="__main__":
-    node1 = TreeNode(1)
-    node2 = TreeNode(2)
-    node3 = TreeNode(3)
-    node4 = TreeNode(4)
-    node5 = TreeNode(5)
-    node6 = TreeNode(6)
-    node7 = TreeNode(7)
-    node8 = TreeNode(8)
+	def merge(self, arr, l, mid, r):
+		# 定义一个辅助数组
+		helps = [0] * (r - l + 1)
+		i = 0
+		p1 = l
+		p2 = mid + 1
+		res = 0
+		while p1 <= mid and p2 <= r:
+			res += (r - p2 + 1) if arr[p1] > arr[p2] else 0
+			if arr[p1] > arr[p2]:
+				helps[i] = arr[p1]
+				p1 += 1
+			else:
+				helps[i] = arr[p2]
+				p2 += 1
+			i += 1
 
-    node1.left = node2
-    node1.right = node3
-    node2.left = node4
-    node2.right = node5
-    node3.left = node6
-    node3.right = node7
+		while p1 <= mid:
+			helps[i] = arr[p1]
+			i += 1
+			p1 += 1
 
-    s = Solution()
-    print(s.IsBalanced_Solution(node1))
+		while p2 <= r:
+			helps[i] = arr[p2]
+			p2 += 1
+			i += 1
+		# 将数组拷贝回到原来额数组
+		k = 0
+		while l <= r:
+			arr[l] = helps[k]
+			l += 1
+			k += 1
+		return res
+
+#对数器
+
+	#首先写一个绝对正确的方法
+	def comparator(self,arr):
+		if arr is None or len(arr)<2:
+			return 0
+		res = 0
+		length = len(arr)
+		for i in range(1,length):
+			for j in range(i):
+				if arr[j]>arr[i]:
+					res+=1
+		return res
+
+	#产生一个随机数发生器
+	def generateRandomArray(self,maxSize,maxValue):
+		arr = [0]*maxSize
+		for i in range(maxSize):
+			arr[i] = random.randint(-maxValue,maxValue)
+		return  arr
+
+	def isEqual(self,arr1,arr2):
+		if (arr1 == None and arr2 !=None) or (arr1!=None and arr2==None):
+			return  False
+		if arr1==arr2==None:
+			return  True
+		if len(arr1) !=len(arr2):
+			return  False
+
+		for i in range(len(arr1)):
+			if arr1[i]!=arr2[i]:
+				return  False
+
+		return True
+
+	def printArray(self,arr):
+		if arr==None:
+			return
+		for i in range(len(arr)):
+			print(arr[i]+" ")
+
+	def main(self):
+		testTime=50000
+		maxSize = 100
+		maxValue = 100
+		succeed=True
+		for i in range(testTime):
+			arr1 = self.generateRandomArray(maxSize,maxValue)
+			arr2 = copy.deepcopy(arr1)
+			if  self.InversePairs(arr1)!=self.comparator(arr2):
+				succeed=False
+				print(arr1)
+				print(arr2)
+				break
+		print("Nice!" if succeed else "Fucking")
+
+
+s = Solution()
+s.main()
